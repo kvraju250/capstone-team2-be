0bd16faed6ee32482e45d98585562ae5a4ce370a
kvraju250 <87770475+kvraju250@users.noreply.github.com>
2022-11-10 11:55:49 -0800

    Updated Swagger docs

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -9,22 +9,22 @@ const appointmentController = require("../controllers/appointment.controller")
 
 //post route to create an appointment
 // will want to add jwt to this route
-router.post("/", appointmentController.createAppointment)
+router.post("/", validateJwtMiddleware, appointmentController.createAppointment)
 
 //get route to return all job requests (requires auth)
 // keeping "validateJwtMiddlewear" means this route requires authentication
-router.get("/", appointmentController.getAppointments)
+router.get("/", validateJwtMiddleware, appointmentController.getAppointments)
 
 // this is get route to retuan all job requests by user email
-router.get("/:id", appointmentController.getAppointmentsById)
+router.get("/:id", validateJwtMiddleware, appointmentController.getAppointmentsById)
 
 //get route to return a specific users (requires auth)
 // router.get("/:email", validateJwtMiddleware, userController.getUser)
 
 //put route to update an appointment (requires auth)
-router.put("/:id", appointmentController.updateAppointment)
+router.put("/:id", validateJwtMiddleware, appointmentController.updateAppointment)
 
 //delete route to delete an appointment (requires auth)
-router.delete("/:id", appointmentController.deleteAppointment)
+router.delete("/:id", validateJwtMiddleware, appointmentController.deleteAppointment)
 
 module.exports = router;
